d9a3bd846632d332c28c8d64ba3f5ef0156fba3f
VishaalMali <vishaal.mali98@gmail.com>
2020-03-16 21:34:58 -0500

    Initial commit with working site. Using django in python. Next step is to add the admin login function

---
+++ mysite/main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Tutorial(models.Model):
+	tutorial_title = models.CharField(max_length=200)
+	tutorial_content = models.TextField()
+	tutorial_published = models.DateTimeField("Date Published", default=datetime.now())
+
+	def _str_(self):
+		return self.tutorial_title
+
